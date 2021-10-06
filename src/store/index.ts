beccaacad93b54879a547acbdf0bba7222e74d55
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-06 15:29:12 -0700

    feat: preload game options

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { preloadedState } from './preload';
 import session from './ducks/session.duck';
+import game from './ducks/game.duck';
 
 export const store = configureStore({
   reducer: {
     session,
+    game,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  const { game } = store.getState();
+  localStorage.setItem('thock-game-options', JSON.stringify(game));
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/store/preload/index.ts
@@ -0,0 +1,11 @@
+import type { GameState } from '../ducks/game.duck';
+
+const gameOptions = localStorage.getItem('thock-game-options');
+const savedState = gameOptions ? JSON.parse(gameOptions) : {};
+
+export const preloadedState = {
+  game: {
+    words: savedState.words,
+    timer: savedState.timer,
+  } as GameState,
+};
