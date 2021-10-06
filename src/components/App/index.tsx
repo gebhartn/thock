8e71a468a804c40d18c7d5f609b3eb3f5be2fb55
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-05 19:18:24 -0700

    feat: application scaffolding

---
+++ src/components/App/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toHumanTime } from '../../helpers';
+
+import { useInterval } from '../../hooks';
+import { increment, selectDuration } from '../../store/ducks/session.duck';
+
+import * as S from './App';
+
+export const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const duration = useSelector(selectDuration);
+
+  useInterval(() => {
+    dispatch(increment());
+  }, 1000);
+
+  return (
+    <S.Container>
+      <code>{toHumanTime(duration)}</code>
+    </S.Container>
+  );
+};
